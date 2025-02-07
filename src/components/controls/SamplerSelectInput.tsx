47bea5a2ef054b4f5ba25686858844681e6921c8
rkfg <rkfg@rkfg.me>
2025-02-07 13:51:51 +0300

    Add more samplers

--- src/components/controls/SamplerSelectInput.tsx
+++ src/components/controls/SamplerSelectInput.tsx
@@ -7,6 +7,8 @@ export const SamplerSelectInput = ({ ...props }: CustomSelectInputProps) => {
                 { text: 'Euler', value: 'euler' },
                 { text: 'Euler A', value: 'euler_ancestral' },
                 { text: 'DPM++ 2M', value: 'dpmpp_2m' },
+                { text: 'Gradient estimation', value: 'gradient_estimation' },
+                { text: 'IPNDM', value: 'ipndm' },
             ]}
             defaultValue='dpmpp_2m'
             {...props}
