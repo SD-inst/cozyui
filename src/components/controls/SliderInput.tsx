ce736b626c93418aebb2dea73fffa09ea3992dcd
rkfg <rkfg@rkfg.me>
2025-02-16 15:01:34 +0300

    Editable slider values

--- src/components/controls/SliderInput.tsx
+++ src/components/controls/SliderInput.tsx
@@ -1,6 +1,7 @@
-import { Box, Slider, SliderProps, Typography } from '@mui/material';
+import { Box, Slider, SliderProps, TextField, Typography } from '@mui/material';
 import { useController } from 'react-hook-form';
 import { HelpButton } from './HelpButton';
+import { useState } from 'react';
 
 export type SliderInputProps = {
     label?: string;
@@ -17,13 +18,42 @@ export const SliderInput = ({
         name: props.name!,
         defaultValue: defaultValue,
     });
+    const [edit, setEdit] = useState(false);
     return (
         <Box width='100%' sx={{ position: 'relative', ...sx }}>
             {/* relative position to align the help elements inside the box */}
-            <Typography width='100%'>
-                {props.label || props.name}: {ctl.field.value}
+            <Box width='100%' display='flex' alignItems='center' gap={1}>
+                <Typography variant='body1'>
+                    {props.label || props.name}:
+                </Typography>
+                {edit ? (
+                    <TextField
+                        autoFocus
+                        size='small'
+                        type='number'
+                        slotProps={{
+                            htmlInput: {
+                                max: props.max,
+                                min: props.min,
+                                step: props.step,
+                            },
+                        }}
+                        {...ctl.field}
+                        onBlur={() => setEdit(false)}
+                        onFocus={(e) => e.target.select()}
+                        onKeyUp={(e) => e.key === 'Enter' && setEdit(false)}
+                    />
+                ) : (
+                    <Typography
+                        variant='body1'
+                        sx={{ cursor: 'pointer' }}
+                        onClick={() => setEdit(true)}
+                    >
+                        {ctl.field.value}
+                    </Typography>
+                )}
                 {tooltip && <HelpButton title={tooltip} />}
-            </Typography>
+            </Box>
             <Slider valueLabelDisplay='auto' {...ctl.field} {...props} />
         </Box>
     );
