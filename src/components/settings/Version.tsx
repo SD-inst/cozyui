34d90f4db53f73cc5e199790957669ee57b49759
rkfg <rkfg@rkfg.me>
2025-03-19 16:08:28 +0300

    Better version info

--- src/components/settings/Version.tsx
+++ src/components/settings/Version.tsx
@@ -4,7 +4,7 @@ import { grey } from '@mui/material/colors';
 export const Version = () => {
     return (
         <Typography variant='body2' align='right' color={grey[700]}>
-            v. {import.meta.env.VITE_APP_VERSION}
+            version: {import.meta.env.VITE_APP_VERSION}
         </Typography>
     );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import git from 'git-rev-sync';
-process.env.VITE_APP_VERSION = git.short('.') + (git.isDirty() ? '-dirty' : '');
+process.env.VITE_APP_VERSION = git.tag(true);
 
 // https://vite.dev/config/
 export default defineConfig({
