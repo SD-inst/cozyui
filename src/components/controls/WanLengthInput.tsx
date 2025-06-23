9e6ddba4c86d0cc411ab97fe417a13217aedbbe5
rkfg <rkfg@rkfg.me>
2025-06-23 13:11:53 +0300

    Make wan longer

--- src/components/controls/WanLengthInput.tsx
+++ src/components/controls/WanLengthInput.tsx
@@ -2,6 +2,6 @@ import { HYLengthInput } from './HYLengthInput';
 
 export const WanLengthInput = ({ ...props }) => {
     return (
-        <HYLengthInput defaultValue={97} fps={16} max={[97, 201]} {...props} />
+        <HYLengthInput defaultValue={97} fps={16} max={[97, 257]} {...props} />
     );
 };
