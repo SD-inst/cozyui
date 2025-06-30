49596bd3f65d6113950a18af9f3d80e8d3fb5264
rkfg <rkfg@rkfg.me>
2025-06-30 20:09:09 +0300

    Change default length to 81

--- src/components/controls/WanLengthInput.tsx
+++ src/components/controls/WanLengthInput.tsx
@@ -2,6 +2,6 @@ import { HYLengthInput } from './HYLengthInput';
 
 export const WanLengthInput = ({ ...props }) => {
     return (
-        <HYLengthInput defaultValue={97} fps={16} max={[97, 257]} {...props} />
+        <HYLengthInput defaultValue={81} fps={16} max={[97, 257]} {...props} />
     );
 };
