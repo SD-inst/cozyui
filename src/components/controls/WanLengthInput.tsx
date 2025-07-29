1eb2ee92271b9d7eed25cbbed15c47a9fb58e373
rkfg <rkfg@rkfg.me>
2025-07-29 11:39:36 +0300

    Increase max Wan length without RIFLEx

--- src/components/controls/WanLengthInput.tsx
+++ src/components/controls/WanLengthInput.tsx
@@ -2,6 +2,6 @@ import { HYLengthInput } from './HYLengthInput';
 
 export const WanLengthInput = ({ ...props }) => {
     return (
-        <HYLengthInput defaultValue={81} fps={16} max={[97, 257]} {...props} />
+        <HYLengthInput defaultValue={81} fps={16} max={[129, 257]} {...props} />
     );
 };
