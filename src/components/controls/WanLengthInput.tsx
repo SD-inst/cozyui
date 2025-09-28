a5a7981784ef4e2e9e3d0bb8d8aa2c5a18c393c3
rkfg <rkfg@rkfg.me>
2025-09-28 12:25:23 +0300

    Allow longer Wan video for loops

--- src/components/controls/WanLengthInput.tsx
+++ src/components/controls/WanLengthInput.tsx
@@ -1,7 +1,17 @@
-import { HYLengthInput } from './HYLengthInput';
+import { useWatch } from 'react-hook-form';
+import { LengthInput } from './LengthSlider';
 
 export const WanLengthInput = ({ ...props }) => {
+    const longVideo = useWatch({ name: ['riflex', 'loop'] });
     return (
-        <HYLengthInput defaultValue={81} fps={16} max={[129, 257]} {...props} />
+        <LengthInput
+            min={1}
+            max={longVideo[0] || longVideo[1]?.enabled ? 257 : 129}
+            step={4}
+            fps={16}
+            defaultValue={81}
+            name='length'
+            {...props}
+        />
     );
 };
