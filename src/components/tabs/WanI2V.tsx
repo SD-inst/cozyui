f33ead3ec1232e24237309c471cd3e63dfb6b63f
rkfg <rkfg@rkfg.me>
2025-10-17 23:28:01 +0300

    Add EasyCache to Wan I2V/T2V

--- src/components/tabs/WanI2V.tsx
+++ src/components/tabs/WanI2V.tsx
@@ -25,6 +25,7 @@ import { WanSampler } from '../controls/WanSampler';
 import { KJWanBlockSwapInput } from '../controls/KJWanBlockSwapInput';
 import { KJWanLoopInput } from '../controls/KJWanLoopInput';
 import { VideoInterpolationSlider } from '../controls/VideoInterpolationSlider';
+import { KJEasyCacheInput } from '../controls/KJEasyCacheInput';
 
 const Content = () => {
     const sflora = useWatch({ name: 'self_forcing_lora', defaultValue: true });
@@ -58,6 +59,7 @@ const Content = () => {
                         defaultValue='过曝，静态，细节模糊不清，字幕，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走'
                     />
                     <WanNAG name='nag' />
+                    <KJEasyCacheInput defaultThreshold={0} defaultStart={6} />
                     <KJWanBlockSwapInput name='block_swap' />
                     <KJWanLoopInput name='loop' sx={{ mt: 2, mb: 2 }} />
                     <SliderInput
